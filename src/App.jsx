e53a50281da5c6f2334be9b98df73a2f67c7fb4a
Gefferson Casasola <gefferson.casasola@outlook.com>
2026-09-16 14:19:40 -0500

    feat: add delete all contacts action

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,10 @@ export default function App() {
     setContacts(updatedContacts);
   }
 
+  function handleDeleteAllContacts() {
+    setContacts([]);
+  }
+
   return (
     <div>
       <Header />
@@ -63,21 +67,89 @@ export default function App() {
         Total: {contacts.length} contactos | Favoritos: {favoriteCount}
       </p>
       <main>
-        <button
-          onClick={handleAddContact}
+        <div
           style={{
-            marginBottom: "1rem",
-            padding: "0.5rem 1rem",
-            fontSize: "1rem",
-            backgroundColor: "#4CAF50",
-            color: "white",
-            border: "none",
-            borderRadius: "4px",
-            cursor: "pointer",
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          + Agregar Contacto
-        </button>
+          <button
+            onClick={handleAddContact}
+            style={{
+              marginBottom: "1rem",
+              padding: "0.5rem 1rem",
+              fontSize: "1rem",
+              backgroundColor: "#4CAF50",
+              color: "white",
+              border: "none",
+              borderRadius: "4px",
+              cursor: "pointer",
+              display: "flex",
+              alignItems: "center",
+              gap: "0.5rem",
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="icon icon-tabler icons-tabler-outline icon-tabler-users-plus"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+              <path d="M3 21v-2a4 4 0 0 1 4 -4h4c.96 0 1.84 .338 2.53 .901" />
+              <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+              <path d="M16 19h6" />
+              <path d="M19 16v6" />
+            </svg>
+            Agregar Contacto
+          </button>
+          <button
+            onClick={handleDeleteAllContacts}
+            style={{
+              marginBottom: "1rem",
+              padding: "0.5rem 1rem",
+              fontSize: "1rem",
+              backgroundColor: "#ff4444",
+              color: "white",
+              border: "none",
+              borderRadius: "4px",
+              cursor: "pointer",
+              display: "flex",
+              alignItems: "center",
+              gap: "0.5rem",
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="#ffffff"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="icon icon-tabler icons-tabler-outline icon-tabler-trash-x"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 7h16" />
+              <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
+              <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
+              <path d="M10 12l4 4m0 -4l-4 4" />
+            </svg>
+            Eliminar Todos los Contactos
+          </button>
+        </div>
+
         <ContactList
           contacts={contacts}
           onDeleteContact={handleDeleteContact}
